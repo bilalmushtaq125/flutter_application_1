97151d400621f8ac649c767d2b08c05857e5f5b3
bilal <bilalbhai62@yahoo.com>
2021-12-21 10:34:46 +0500

    today

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const appTitle = 'CMS';
+  static const appTitle = 'CMSS';
 
   @override
   Widget build(BuildContext context) {
